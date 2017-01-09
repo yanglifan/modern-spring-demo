1ccafa2ba0d0d229d7ef88517bf0e4804be4ad43
杨理帆 <yanglifan@gmail.com>
2017-01-09 14:29:06 +0800

    Provide a simple Spring MVC unit test.

---
+++ src/test/java/com/github/yanglifan/demo/modernspring/UserRestControllerTest.java
@@ -0,0 +1,44 @@
+package com.github.yanglifan.demo.modernspring;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserRestControllerTest {
+    public static final String USERNAME = "yanglifan";
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        userRepository.save(new User(USERNAME));
+    }
+
+    @Test
+    public void getByName() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/users/" + USERNAME)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void create() throws Exception {
+
+    }
+
+}
