69b6b2071f0af2a33d528368f318e27739b02e24
Lifan Yang <yanglifan@gmail.com>
2017-01-15 15:49:21 +0800

    Fix #19: Demo Spring Boot Type-safe Configuration Properties

--- src/main/java/com/github/yanglifan/demo/modernspring/ModernSpringDemoApplication.java
+++ src/main/java/com/github/yanglifan/demo/modernspring/ModernSpringDemoApplication.java
@@ -1,14 +1,20 @@
 package com.github.yanglifan.demo.modernspring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
 import java.util.Arrays;
 
 @SpringBootApplication
 public class ModernSpringDemoApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModernSpringDemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ModernSpringDemoApplication.class, args);
@@ -24,5 +30,36 @@ public class ModernSpringDemoApplication {
             orderRepository.save(new Order("2017011313591000001", stark.getId()));
             orderRepository.save(new Order("2017011313591000002", rogers.getId()));
         };
+    }
+
+    @Bean
+    CommandLineRunner printProperties(FooProperties fooProperties) {
+        return args -> {
+            LOGGER.info("foo.enabled={}", fooProperties.isEnabled());
+            LOGGER.info("foo.remoteAddress={}", fooProperties.getRemoteAddress());
+        };
+    }
+
+    @ConfigurationProperties("foo")
+    @Component
+    static class FooProperties {
+        private boolean enabled;
+        private InetAddress remoteAddress;
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public InetAddress getRemoteAddress() {
+            return remoteAddress;
+        }
+
+        public void setRemoteAddress(InetAddress remoteAddress) {
+            this.remoteAddress = remoteAddress;
+        }
     }
 }
