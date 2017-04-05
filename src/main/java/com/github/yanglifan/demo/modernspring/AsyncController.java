2a4615c9d9f6f93265a9213f2ecd07f2f69150dd
杨理帆 <yanglifan@gmail.com>
2017-04-05 12:59:26 +0800

    Provide a simple Spring Async MVC (DeferredResult) demo

---
+++ src/main/java/com/github/yanglifan/demo/modernspring/AsyncController.java
@@ -0,0 +1,31 @@
+package com.github.yanglifan.demo.modernspring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RequestMapping("async")
+@RestController
+public class AsyncController {
+
+    private static final String DEFERRED_RESULT = "deferred-result-";
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    private AtomicInteger count = new AtomicInteger();
+
+    @GetMapping("simpleDeferredResult")
+    public DeferredResult<String> simpleDeferredResult() {
+        final DeferredResult<String> simpleDeferredResult = new DeferredResult<>();
+        scheduler.schedule(
+                () -> simpleDeferredResult.setResult(DEFERRED_RESULT + count.incrementAndGet()),
+                30, TimeUnit.SECONDS
+        );
+        return simpleDeferredResult;
+    }
+}
