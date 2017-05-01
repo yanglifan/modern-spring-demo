13e2a2552cd3c889ac763894ac1ec565d5192177
Lifan Yang <yanglifan@gmail.com>
2017-05-01 12:50:29 +0800

    #22 Implement Zuul proxy in one app

---
+++ core-service/src/main/java/com/github/yanglifan/workshop/spring/gateway/GatewayConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.yanglifan.workshop.spring.gateway;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+
+@EnableZuulProxy
+@Configuration
+public class GatewayConfiguration {
+}
