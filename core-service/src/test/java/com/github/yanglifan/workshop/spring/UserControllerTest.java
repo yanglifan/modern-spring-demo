a40f2511a2b4c2880b51a3511f96d158d009fb04
杨理帆 <yanglifan@gmail.com>
2017-05-03 10:33:54 +0800

    #27 Provide the basic configuration and demo of Spring RestDocs

--- core-service/src/test/java/com/github/yanglifan/workshop/spring/UserControllerTest.java
+++ core-service/src/test/java/com/github/yanglifan/workshop/spring/UserControllerTest.java
@@ -2,16 +2,22 @@ package com.github.yanglifan.workshop.spring;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,10 +25,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
     private static final String USERNAME = "yanglifan";
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
 
     @Autowired
     private UserRepository userRepository;
@@ -30,14 +42,19 @@ public class UserControllerTest {
     @Before
     public void setUp() throws Exception {
         userRepository.save(new User(USERNAME));
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .build();
     }
 
     @Test
     public void getByName() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/users/" + USERNAME)
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/" + USERNAME)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value(USERNAME));
+                .andExpect(jsonPath("$.name").value(USERNAME))
+                .andDo(document("index"));
     }
 
     @Test
