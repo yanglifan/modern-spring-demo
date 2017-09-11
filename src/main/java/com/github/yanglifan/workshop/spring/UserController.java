3e0f3ec57f73a5956da3b942f3b4eb118dc0ed98
Yang Lifan <yanglifan@gmail.com>
2017-09-11 18:00:07 +0800

    An unused Spring async sample

--- src/main/java/com/github/yanglifan/workshop/spring/UserController.java
+++ src/main/java/com/github/yanglifan/workshop/spring/UserController.java
@@ -4,13 +4,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.concurrent.CompletableFuture;
 
 @Api(value = "User APIs", description = "Operations of users")
 @RequestMapping("/users")
@@ -35,5 +38,17 @@ public class UserController {
     public Boolean create(@RequestBody @Valid User user) {
         userRepository.save(user);
         return Boolean.TRUE;
+    }
+
+    RestTemplate restTemplate;
+
+    @Async
+    public CompletableFuture<User> findUser(String user)
+            throws InterruptedException {
+        String url = String.format("https://api.github.com/users/%s", user);
+        User results = restTemplate.getForObject(url, User.class);
+        // Artificial delay of 1s for demonstration purposes
+        Thread.sleep(1000L);
+        return CompletableFuture.completedFuture(results);
     }
 }
