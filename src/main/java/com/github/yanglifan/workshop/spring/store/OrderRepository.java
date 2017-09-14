0aa5b3efc84f1663a4e7b77a35b355aa6fa65a0b
Yang Lifan <yanglifan@gmail.com>
2017-09-14 19:12:58 +0800

    Custom a span in a internal repository method.

--- src/main/java/com/github/yanglifan/workshop/spring/store/OrderRepository.java
+++ src/main/java/com/github/yanglifan/workshop/spring/store/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.github.yanglifan.workshop.spring.store;
 
-import com.github.yanglifan.workshop.spring.store.Order;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends PagingAndSortingRepository<Order, String> {
+    @NewSpan
+    Order save(Order order);
 }
