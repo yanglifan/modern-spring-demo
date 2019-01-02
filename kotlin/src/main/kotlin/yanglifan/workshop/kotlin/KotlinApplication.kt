b408f9ba7d0287f32e5bc26bb93f94a3c1b8800f
Yang Lifan <yanglifan@gmail.com>
2019-01-02 17:40:27 +0800

    增加 Kotlin Demo 模块

---
+++ kotlin/src/main/kotlin/yanglifan/workshop/kotlin/KotlinApplication.kt
@@ -0,0 +1,11 @@
+package yanglifan.workshop.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinApplication>(*args)
+}
