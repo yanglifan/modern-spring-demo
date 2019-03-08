1ed249a54cb9dd10d57b22a9dca5fc4a10044cd3
Yang Lifan <yanglifan@gmail.com>
2019-03-08 19:02:19 +0800

    增加 Filter 处理

--- spring-reactor/src/main/java/yanglifan/workshop/spring/reactor/SourceCodeStudy.java
+++ spring-reactor/src/main/java/yanglifan/workshop/spring/reactor/SourceCodeStudy.java
@@ -5,6 +5,7 @@ import reactor.core.publisher.Flux;
 public class SourceCodeStudy {
     public static void main(String[] args) {
         Flux.just("tom", "jack", "allen")
+                .filter(s -> s.length() > 3)
                 .map(s -> s.concat("@qq.com"))
                 .subscribe(System.out::println);
     }
